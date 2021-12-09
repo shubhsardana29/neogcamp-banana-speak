50e85b0e55a67ef39146c67b8a74856b049a759b
shubhsardana29 <52607235+shubhsardana29@users.noreply.github.com>
2021-12-09 17:12:40 +0530

    updated css-selector

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+var outputDiv = document.querySelector("#translate-output");
 
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
